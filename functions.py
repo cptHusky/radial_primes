56053b1552b2ec6fb9b15ad9dc1948b00319a366
Petr Alekseev <petr.alekseev@ntprog.com>
2022-04-04 09:33:04 +0300

    main drawing core basicly done

---
+++ functions.py
@@ -0,0 +1,25 @@
+import math
+
+
+def polar_to_decart(ro, fi):
+    x = ro * math.cos(fi)
+    y = ro * math.sin(fi)
+    coordinates = (x, y)
+    return coordinates
+
+
+def prime_gen(limit):
+    number = 1
+    counter = 1
+    primes = [2, ]
+    while number < limit:
+        number += 2
+        is_prime = True
+        for n in range(counter):
+            if number % primes[n] == 0:
+                is_prime = False
+        if is_prime:
+            primes.append(number)
+            counter += 1
+            print(f'{number} is prime')
+    return primes
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-from PIL import Image, ImageDraw        # use pip install Pillow
+from PIL import Image, ImageDraw        # pip install Pillow
+from functions import polar_to_decart
 
 
 if __name__ == '__main__':
     # side = int(input())
-    side = 800
-    back = Image.new('RGB', (side, side))
-    h = back.height
-    w = back.width
-    h_half = back.height / 2
-    w_half = back.width / 2
+    h = 800
+    w = 600
+    h_half = h / 2
+    w_half = w / 2
+    back = Image.new('RGB', (h, w))
     draw = ImageDraw.Draw(back)
-    draw.line((h_half, 0) + (h_half, w), fill=128)
-    draw.line((0, w_half) + (h, w_half), fill=128)
+    draw.line((h_half, 0) + (h_half, w), fill=(63, 127, 255))
+    draw.line((0, w_half) + (h, w_half), fill=(63, 63, 255))
+    step = 5
+    for i in range(10000):
+        point_pre_coordinates = polar_to_decart(i, i)
+        point_coordinates = (point_pre_coordinates[0] +
+                             h_half, point_pre_coordinates[1] + w_half)
+        draw.point(point_coordinates, fill=(255, 255, 255))
+    print(point_coordinates)
+
     back.show()
-    # print(f'{back.size}\n{type(back.size)}')
+# add colouring for different rows or spirals
